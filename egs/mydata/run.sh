0a279ecbbd1bb56493e03af3e1857c05e08e1266
panxin <panxinecho@qq.com>
2023-02-26 18:41:02 +0800

    write train main bone

--- egs/mydata/run.sh
+++ egs/mydata/run.sh
@@ -4,12 +4,18 @@ source path.sh
 set -eu
 set -x
 
-start_stage=0
-end_stage=0
+start_stage=1
+end_stage=99
 
 wav_dir=$1
 feat_dir=$2
 
-if [ ${start_stage} -ge 0 -a ${end_stage} -le 0 ]; then
+if [ ${start_stage} -le 0 -a ${end_stage} -ge 0 ]; then
     bash local/preprocess.sh ${wav_dir} ${feat_dir} || exit 1
 fi
+
+if [ ${start_stage} -le 1 -a ${end_stage} -ge 1 ]; then
+    python ${SRC_ROOT}/train.py ${feat_dir} --save-dir ./ckpts || exit1
+fi
+
+echo "All done!"
